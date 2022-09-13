34ba8d236fa2e0978e211f7c549eaf1baf592dbc
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-09-13 14:23:03 +0200

    Made it so that the user gets a "good job" message

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,5 +1,7 @@
 <template>
   <p>My score: {{ score }}</p>
+  <p v-if="!slowReaction">Fast reaction</p>
+  <p v-else>Slow reaction</p>
 </template>
 
 <script>
@@ -7,8 +9,19 @@ export default {
   props: ["score"],
   data() {
     return {
-      result: 0,
+      slowReaction: false
     };
   },
+  mounted(){
+    this.determineReaction(this.score)
+  },
+  methods: {
+    determineReaction(score){
+        if(score > 500){
+            console.log("higher")
+            this.slowReaction = true;
+        }
+    }
+  }
 };
 </script>
