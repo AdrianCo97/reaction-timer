4a260bc3d1d3ceddbb2ffd3bf35c3f50141fc80b
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-09-13 13:47:49 +0200

    The timer now stops once the user clicks on the block

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="block" v-if="showBlock">
+    <div class="block" v-if="showBlock" @click="stopTimer">
         click me
     </div>
+
+    
 </template>
 
 <script>
@@ -9,20 +11,26 @@
         props: ['delay'],
         data(){
             return {
-                showBlock: false
+                showBlock: false,
+                timer: null,
+                reactionTime: 0
             }
         },
         mounted() {
-            console.log('component mounted')
             setTimeout(() => {
                 this.showBlock = true
+                this.startTimer()
             }, this.delay)
-        },
-        updated(){
-            console.log("component updated")
         }, 
-        unmounted() {
-            console.log("component unmounted")
+        methods: {
+            startTimer(){
+                this.timer = setInterval(() => {
+                    this.reactionTime += 10
+                }, 10)
+            }, 
+            stopTimer(){
+                clearInterval(this.timer)
+            }
         }
     }
 </script>
